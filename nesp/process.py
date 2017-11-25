b6790b51a17560236539ea8508030742a2d06114
James Watmuff <james@planticle.com.au>
2017-11-25 15:57:20 +1100

    Processing script improvements:  - add more export types to facilitate manual checking  - add 'all' task that runs all processing steps

--- nesp/process.py
+++ nesp/process.py
@@ -13,6 +13,7 @@ import logging
 import os
 from nesp.geo import to_multipolygon
 from nesp.db import get_session
+from shapely.geometry import Point
 
 log = logging.getLogger(__name__)
 
@@ -34,10 +35,11 @@ def main():
     subparsers = parser.add_subparsers(help = 'command', dest = 'command')
 
     p = subparsers.add_parser('alpha_hull')
-    p = subparsers.add_parser('export_alpha_hull')
+    p = subparsers.add_parser('export') # TODO - add sub-options for export
     p = subparsers.add_parser('range_ultrataxon')
     p = subparsers.add_parser('pseudo_absence')
     p = subparsers.add_parser('response_variable')
+    p = subparsers.add_parser('all')
 
     args = parser.parse_args()
 
@@ -53,58 +55,208 @@ def main():
 
     if args.command == 'alpha_hull':
         nesp.processing.alpha_hull.process_database(species = species, commit = args.commit)
-    elif args.command == 'export_alpha_hull':
-        export_alpha_hull()
+    elif args.command == 'export':
+        export(species = species)
     elif args.command == 'range_ultrataxon':
         nesp.processing.range_ultrataxon.process_database(species = species, commit = args.commit)
     elif args.command == 'pseudo_absence':
         nesp.processing.pseudo_absence.process_database(commit = args.commit)
     elif args.command == 'response_variable':
         nesp.processing.response_variable.process_database(commit = args.commit)
+    elif args.command == 'all':
+        if not args.commit:
+            log.error("Dry-run mode not supported for 'all'")
+            return
+        if args.species:
+            log.error("Passing species not supported for 'all'")
+            return
 
-# ----- Export alpha hull
+        log.info("STEP 1 - ALPHA HULLS")
+        nesp.processing.alpha_hull.process_database(commit = True)
 
-def export_alpha_hull():
+        log.info("STEP 2 - RANGE AND ULTRATAXON DEFINITION")
+        nesp.processing.range_ultrataxon.process_database(commit = True)
+
+        log.info("STEP 3 - GENERATE PSEUDO ABSENCES")
+        nesp.processing.pseudo_absence.process_database(commit = True)
+
+        log.info("PROCESSING COMPLETE")
+
+# ----- Export shapefiles showing processed data
+
+def export(species = None):
     session = get_session()
 
-    for spno in tqdm(get_all_spno(session)):
-        filename = os.path.join(nesp.config.data_dir('spatial'), 'alpha-%s.shp' % spno)
+    # export_alpha = True
+    # export_ultrataxa = True
+    # export_pseudo_absence = True
+    # export_grid = False
 
-        alpha_hulls = session.execute("""SELECT taxon_id, range_id, breeding_range_id, ST_AsWKB(geometry)
-                FROM taxon_presence_alpha_hull, taxon
-                WHERE taxon_id = taxon.id
-                AND spno = :spno""", {
+    export_alpha = True
+    export_ultrataxa = False
+    export_pseudo_absence = False
+    export_grid = True
+
+    export_dir = nesp.config.data_dir('export')
+
+    if species == None:
+        species = get_all_spno(session)
+
+    for spno in tqdm(species):
+        if export_alpha:
+            filename = os.path.join(export_dir, '%s-alpha.shp' % spno)
+
+            alpha_hulls = session.execute("""SELECT taxon_id, range_id, breeding_range_id, ST_AsWKB(geometry)
+                    FROM taxon_presence_alpha_hull, taxon
+                    WHERE taxon_id = taxon.id
+                    AND spno = :spno""", {
+                        'spno': spno
+                    }).fetchall()
+
+            if len(alpha_hulls) > 0:
+
+                with fiona.open(filename, 'w',
+                    driver='ESRI Shapefile',
+                    crs={'no_defs': True, 'ellps': 'WGS84', 'datum': 'WGS84', 'proj': 'longlat'},
+                    schema={
+                        'geometry': 'MultiPolygon',
+                        'properties': {
+                            'TAXONID': 'str',
+                            'RNGE': 'int',
+                            'BRRNGE': 'int'
+                        }
+                    }) as output:
+
+                    for taxon_id, range_id, breeding_range_id, geom_wkb in alpha_hulls:
+                        geom = to_multipolygon(shapely.wkb.loads(geom_wkb))
+
+                        if len(geom) == 0:
+                            continue
+
+                        output.write({
+                            'geometry': shapely.geometry.mapping(geom),
+                            'properties': {
+                                'TAXONID': taxon_id,
+                                'RNGE': range_id,
+                                'BRRNGE': breeding_range_id
+                            }
+                        })
+
+        if export_ultrataxa:
+            filename = os.path.join(export_dir, '%s-ultrataxa.shp' % spno)
+
+            items = session.execute("""SELECT ST_X(coords) AS x, ST_Y(coords) AS y, taxon.id, range_id, generated_subspecies, t2_survey_site.site_id, t2_survey.search_type_id
+                FROM t2_ultrataxon_sighting, t2_sighting, taxon, t2_survey
+                LEFT JOIN t2_survey_site ON t2_survey_site.survey_id = t2_survey.id
+                WHERE t2_ultrataxon_sighting.sighting_id = t2_sighting.id
+                AND t2_sighting.survey_id = t2_survey.id
+                AND t2_ultrataxon_sighting.taxon_id = taxon.id
+                AND taxon.spno = :spno
+                """, {
                     'spno': spno
                 }).fetchall()
 
-        if len(alpha_hulls) > 0:
+            with fiona.open(filename, 'w',
+                    driver='ESRI Shapefile',
+                    crs={'no_defs': True, 'ellps': 'WGS84', 'datum': 'WGS84', 'proj': 'longlat'},
+                    schema={
+                        'geometry': 'Point',
+                        'properties': {
+                            'TaxonID': 'str',
+                            'Rnge': 'int',
+                            'Generated': 'int',
+                            'SearchType': 'int',
+                            'SiteID': 'int'
+                        }
+                    }) as output:
+
+                    for x, y, taxon_id, range_id, generated_subspecies, site_id, search_type_id in items:
+                        geom = Point(x, y)
+
+                        output.write({
+                            'geometry': shapely.geometry.mapping(geom),
+                            'properties': {
+                                'TaxonID': taxon_id,
+                                'Rnge': range_id,
+                                'Generated': generated_subspecies,
+                                'SearchType': search_type_id,
+                                'SiteID': site_id
+                            }
+                        })
+
+        if export_pseudo_absence:
+            filename = os.path.join(export_dir, '%s-pa.shp' % spno)
+
+            items = session.execute("""SELECT ST_X(coords) AS x, ST_Y(coords) AS y, taxon.id
+                FROM t2_processed_sighting, t2_processed_survey, t2_survey, taxon
+                WHERE t2_processed_sighting.survey_id = t2_processed_survey.id
+                AND t2_processed_survey.raw_survey_id = t2_survey.id
+                AND t2_processed_sighting.taxon_id = taxon.id
+                AND taxon.spno = :spno
+                AND pseudo_absence
+                AND experimental_design_type_id = 1
+                """, {
+                    'spno': spno
+                }).fetchall()
 
             with fiona.open(filename, 'w',
-                driver='ESRI Shapefile',
-                crs={'no_defs': True, 'ellps': 'WGS84', 'datum': 'WGS84', 'proj': 'longlat'},
-                schema={
-                    'geometry': 'MultiPolygon',
-                    'properties': {
-                        'TAXONID': 'str',
-                        'RNGE': 'int',
-                        'BRRNGE': 'int'
-                    }
-                }) as output:
-
-                for taxon_id, range_id, breeding_range_id, geom_wkb in alpha_hulls:
-                    geom = to_multipolygon(shapely.wkb.loads(geom_wkb))
-
-                    if len(geom) == 0:
-                        continue
-
-                    output.write({
-                        'geometry': shapely.geometry.mapping(geom),
+                    driver='ESRI Shapefile',
+                    crs={'no_defs': True, 'ellps': 'WGS84', 'datum': 'WGS84', 'proj': 'longlat'},
+                    schema={
+                        'geometry': 'Point',
                         'properties': {
-                            'TAXONID': taxon_id,
-                            'RNGE': range_id,
-                            'BRRNGE': breeding_range_id
+                            'TaxonID': 'str'
                         }
-                    })
+                    }) as output:
+
+                    for x, y, taxon_id in items:
+                        geom = Point(x, y)
+
+                        output.write({
+                            'geometry': shapely.geometry.mapping(geom),
+                            'properties': {
+                                'TaxonID': taxon_id
+                            }
+                        })
+
+        if export_grid:
+            filename = os.path.join(export_dir, '%s-grid.shp' % spno)
+
+            items = session.execute("""SELECT ST_X(coords) AS x, ST_Y(coords) AS y, grid_cell_id, taxon.id, pseudo_absence
+                FROM t2_processed_sighting, t2_processed_survey, t2_survey, taxon
+                WHERE t2_processed_sighting.survey_id = t2_processed_survey.id
+                AND t2_processed_survey.raw_survey_id = t2_survey.id
+                AND t2_processed_sighting.taxon_id = taxon.id
+                AND taxon.spno = :spno
+                AND experimental_design_type_id = 2
+                """, {
+                    'spno': spno
+                }).fetchall()
+
+            with fiona.open(filename, 'w',
+                    driver='ESRI Shapefile',
+                    crs={'no_defs': True, 'ellps': 'WGS84', 'datum': 'WGS84', 'proj': 'longlat'},
+                    schema={
+                        'geometry': 'Point',
+                        'properties': {
+                            'TaxonID': 'str',
+                            'GridID': 'int',
+                            'Pseudo': 'int'
+                        }
+                    }) as output:
+
+                    for x, y, grid_cell_id, taxon_id, pseudo_absence in items:
+                        geom = Point(x, y)
+
+                        output.write({
+                            'geometry': shapely.geometry.mapping(geom),
+                            'properties': {
+                                'TaxonID': taxon_id,
+                                'GridID': grid_cell_id,
+                                'Pseudo': pseudo_absence
+                            }
+                        })
+
 
 def get_all_spno(session):
     return [spno for (spno,) in session.execute("SELECT DISTINCT spno FROM taxon").fetchall()]
