76fabfa12436cde89c7a5cdf767114f0fb3a5db1
James Watmuff <james@planticle.com.au>
2017-12-06 21:31:01 +1100

    Update region import to include state

--- nesp/import_region.py
+++ nesp/import_region.py
@@ -14,32 +14,33 @@ log = logging.getLogger(__name__)
 def main():
 	logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)-15s %(name)s %(levelname)-8s %(message)s')
 
-	parser = argparse.ArgumentParser(description='Import sub-IBRA regions from shapefile')
-	parser.add_argument('filename', type=str, help='Shapefile containing sub-IBRA regions')
+	parser = argparse.ArgumentParser(description='Import regions from shapefile')
+	parser.add_argument('filename', type=str, help='Shapefile containing regions')
 	args = parser.parse_args()
 
 	session = get_session()
 
 	with fiona.open(args.filename, encoding = 'Windows-1252') as shp:
 		reproject = reproject_fn(pyproj.Proj(shp.crs), pyproj.Proj('+init=EPSG:4326'))
-		for feature in tqdm(shp):
+		for index, feature in enumerate(tqdm(shp)):
 			props = feature['properties']
 
 			geometry = reproject(shape(feature['geometry']))
 
-			session.execute("""INSERT INTO region (id, name, geometry)
-					VALUES (:id, :name, ST_GeomFromWKB(_BINARY :geometry_wkb))""", {
-						'id': props['subIBRA_ID'],
-						'name': props['Name'],
-						'geometry_wkb': shapely.wkb.dumps(to_multipolygon(geometry))
+			session.execute("""INSERT INTO region (id, name, geometry, state)
+					VALUES (:id, :name, ST_GeomFromWKB(_BINARY :geometry_wkb), :state)""", {
+						'id': index,
+						'name': props['RegName'],
+						'geometry_wkb': shapely.wkb.dumps(to_multipolygon(geometry)),
+						'state': props['StateName']
 					})
 
 			for geometry in subdivide_geometry(geometry):
 
 				session.execute("""INSERT INTO region_subdiv (id, name, geometry)
 					VALUES (:id, :name, ST_GeomFromWKB(_BINARY :geometry_wkb))""", {
-						'id': props['subIBRA_ID'],
-						'name': props['Name'],
+						'id': index,
+						'name': props['RegName'],
 						'geometry_wkb': shapely.wkb.dumps(to_multipolygon(geometry))
 					})
 
