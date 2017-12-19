40731c20976452474e7b161fde41f789d175080a
James Watmuff <james@planticle.com.au>
2017-12-20 01:43:17 +1100

    Response variable processing - use processing_method table

--- nesp/processing/response_variable.py
+++ nesp/processing/response_variable.py
@@ -11,10 +11,10 @@ def process_database(species = None, commit = False):
     session = get_session()
 
     if species == None:
-        taxa = [taxon_id for (taxon_id,) in session.execute("SELECT DISTINCT taxon_id FROM t2_processed_sighting").fetchall()]
+        taxa = [taxon_id for (taxon_id,) in session.execute("SELECT DISTINCT taxon_id FROM processing_method WHERE data_type = 2").fetchall()]
     else:
         taxa = [taxon_id for (taxon_id,) in session.execute(
-            "SELECT DISTINCT taxon_id FROM t2_processed_sighting, taxon WHERE taxon.id = taxon_id AND spno IN :species", {
+            "SELECT DISTINCT taxon_id FROM processing_method, taxon WHERE taxon.id = taxon_id AND data_type = 2 AND spno IN :species", {
                 'species': species
             }).fetchall()]
 
@@ -40,100 +40,110 @@ def process_database(species = None, commit = False):
 def aggregate_by_month(taxon_id, commit = False):
     session = get_session()
     try:
-        experimental_design_type_id, response_variable_type_id, positional_accuracy_threshold_in_m = session.execute(
-            """SELECT experimental_design_type_id, response_variable_type_id, positional_accuracy_threshold_in_m
-                FROM taxon
-                WHERE id = :taxon_id""",
-            { 'taxon_id': taxon_id }
-        ).fetchone()
+        sql = """SELECT source_id, MAX(experimental_design_type_id), MAX(response_variable_type_id), MAX(positional_accuracy_threshold_in_m), COUNT(DISTINCT response_variable_type_id, experimental_design_type_id, COALESCE(positional_accuracy_threshold_in_m, 0)) AS cnt
+                FROM processing_method
+                WHERE taxon_id = :taxon_id
+                AND data_type = 2
+                GROUP BY source_id"""
 
-        where_conditions = []
+        for row in session.execute(sql, { 'taxon_id': taxon_id }).fetchall():
 
-        # Tweak SQL based on response variable type
+            source_id, experimental_design_type_id, response_variable_type_id, positional_accuracy_threshold_in_m, count = row
 
-        if response_variable_type_id == 1:
-            aggregate_expression = 'AVG(count)'
-            where_conditions.append("unit_id > 1")
+            if count > 1:
+                log.warning("More than one experimental design type / response variable type / accuracy threshold for taxon %s, source %s" % (taxon_id, source_id))
+                log.warning("Using experimental_design_type_id = %s, response_variable_type_id = %s, positional_accuracy_threshold_in_m = %s" %
+                    (experimental_design_type_id, response_variable_type_id, positional_accuracy_threshold_in_m))
 
-        elif response_variable_type_id == 2:
-            aggregate_expression = 'MAX(count)'
-            where_conditions.append("unit_id > 1")
+            where_conditions = []
 
-        elif response_variable_type_id == 3:
-            aggregate_expression = 'AVG(count > 0)'
+            # Tweak SQL based on response variable type
 
-        # Tweak SQL based on experimental design type
+            if response_variable_type_id == 1:
+                aggregate_expression = 'AVG(count)'
+                where_conditions.append("unit_id > 1")
 
-        if experimental_design_type_id == 1:
-            fields = 'site_id, search_type_id'
-            where_conditions.append("site_id IS NOT NULL")
+            elif response_variable_type_id == 2:
+                aggregate_expression = 'MAX(count)'
+                where_conditions.append("unit_id > 1")
 
-        elif experimental_design_type_id == 2:
-            fields = 'grid_cell_id, search_type_id'
-            where_conditions.append("grid_cell_id IS NOT NULL")
+            elif response_variable_type_id == 3:
+                aggregate_expression = 'AVG(count > 0)'
 
-        elif experimental_design_type_id == 3:
-            fields = 'grid_cell_id'
-            where_conditions.append("grid_cell_id IS NOT NULL")
+            # Tweak SQL based on experimental design type
 
-        # Generate SQL for monthly aggregation
+            if experimental_design_type_id == 1:
+                fields = 'site_id, search_type_id'
+                where_conditions.append("site_id IS NOT NULL")
 
-        sql = """INSERT INTO aggregated_by_month (
-            start_date_y,
-            start_date_m,
-            source_id,
-            {fields},
-            taxon_id,
-            experimental_design_type_id,
-            response_variable_type_id,
-            value,
-            region_id,
-            positional_accuracy_in_m,
-            unit_id,
-            data_type)
-        SELECT
-            start_date_y,
-            start_date_m,
-            source_id,
-            {fields},
-            taxon_id,
-            :experimental_design_type_id,
-            :response_variable_type_id,
-            {aggregate_expression},
-            MIN((SELECT MIN(region_id) FROM tmp_region_lookup t WHERE t.site_id <=> survey.site_id AND t.grid_cell_id <=> survey.grid_cell_id)),
-            MAX((SELECT positional_accuracy_in_m FROM t2_survey WHERE t2_survey.id = raw_survey_id)),
-            :unit_id,
-            2
-        FROM
-            t2_processed_survey survey
-            INNER JOIN t2_processed_sighting sighting ON survey_id = survey.id
-        WHERE taxon_id = :taxon_id
-        AND raw_survey_id IN (
-            SELECT id
-            FROM t2_survey
-            WHERE COALESCE(positional_accuracy_in_m < :positional_accuracy_threshold_in_m, TRUE)
-            AND COALESCE(duration_in_minutes <= 6 * 60, TRUE)
-            AND COALESCE((t2_survey.start_date_y, t2_survey.start_date_m, start_date_d) = (finish_date_y, finish_date_m, finish_date_d), TRUE)
-        )
-        {where_conditions}
-        GROUP BY
-            start_date_y,
-            start_date_m,
-            source_id,
-            {fields}
-        """.format(
-                aggregate_expression = aggregate_expression,
-                where_conditions = " ".join("AND %s" % cond for cond in where_conditions),
-                fields = fields
+            elif experimental_design_type_id == 2:
+                fields = 'grid_cell_id, search_type_id'
+                where_conditions.append("grid_cell_id IS NOT NULL")
+
+            elif experimental_design_type_id == 3:
+                fields = 'grid_cell_id'
+                where_conditions.append("grid_cell_id IS NOT NULL")
+
+            # Generate SQL for monthly aggregation
+
+            sql = """INSERT INTO aggregated_by_month (
+                start_date_y,
+                start_date_m,
+                source_id,
+                {fields},
+                taxon_id,
+                experimental_design_type_id,
+                response_variable_type_id,
+                value,
+                region_id,
+                positional_accuracy_in_m,
+                unit_id,
+                data_type)
+            SELECT
+                start_date_y,
+                start_date_m,
+                source_id,
+                {fields},
+                taxon_id,
+                :experimental_design_type_id,
+                :response_variable_type_id,
+                {aggregate_expression},
+                MIN((SELECT MIN(region_id) FROM tmp_region_lookup t WHERE t.site_id <=> survey.site_id AND t.grid_cell_id <=> survey.grid_cell_id)),
+                MAX((SELECT positional_accuracy_in_m FROM t2_survey WHERE t2_survey.id = raw_survey_id)),
+                :unit_id,
+                2
+            FROM
+                t2_processed_survey survey
+                INNER JOIN t2_processed_sighting sighting ON survey_id = survey.id
+            WHERE taxon_id = :taxon_id
+            AND source_id = :source_id
+            AND raw_survey_id IN (
+                SELECT id
+                FROM t2_survey
+                WHERE COALESCE(positional_accuracy_in_m < :positional_accuracy_threshold_in_m, TRUE)
+                AND COALESCE(duration_in_minutes <= 6 * 60, TRUE)
+                AND COALESCE((t2_survey.start_date_y, t2_survey.start_date_m, start_date_d) = (finish_date_y, finish_date_m, finish_date_d), TRUE)
             )
+            {where_conditions}
+            GROUP BY
+                start_date_y,
+                start_date_m,
+                source_id,
+                {fields}
+            """.format(
+                    aggregate_expression = aggregate_expression,
+                    where_conditions = " ".join("AND %s" % cond for cond in where_conditions),
+                    fields = fields
+                )
 
-        session.execute(sql, {
-            'experimental_design_type_id': experimental_design_type_id,
-            'response_variable_type_id': response_variable_type_id,
-            'taxon_id': taxon_id,
-            'unit_id': 1 if response_variable_type_id == 3 else 2,
-            'positional_accuracy_threshold_in_m': positional_accuracy_threshold_in_m
-        })
+            session.execute(sql, {
+                'experimental_design_type_id': experimental_design_type_id,
+                'response_variable_type_id': response_variable_type_id,
+                'taxon_id': taxon_id,
+                'source_id': source_id,
+                'unit_id': 1 if response_variable_type_id == 3 else 2,
+                'positional_accuracy_threshold_in_m': positional_accuracy_threshold_in_m
+            })
 
         if commit:
             session.commit()
