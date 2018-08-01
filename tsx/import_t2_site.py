fe52505917f05256381b1b6bcf1ab03e780c8c6d
James Watmuff <james@planticle.com.au>
2018-08-01 20:46:33 +1000

    Remove references to Type 3 data

--- tsx/import_t2_site.py
+++ tsx/import_t2_site.py
@@ -14,8 +14,8 @@ log = logging.getLogger(__name__)
 def main():
 	logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)-15s %(name)s %(levelname)-8s %(message)s')
 
-	parser = argparse.ArgumentParser(description='Import Type 2/3 sites from shapefile')
-	parser.add_argument('filename', type=str, help='Shapefile containing type 2/3 site polygons')
+	parser = argparse.ArgumentParser(description='Import Type 2 sites from shapefile')
+	parser.add_argument('filename', type=str, help='Shapefile containing type 2 site polygons')
 	args = parser.parse_args()
 
 	session = get_session()
