487eaa3ed3a80b5e1fae8f32bd2fdde9811b0c1c
James Watmuff <james@planticle.com.au>
2018-08-02 19:07:17 +1000

    Fix SSL issue when downloading sample data

--- setup/download_sample_data.py
+++ setup/download_sample_data.py
@@ -1,11 +1,18 @@
-from urllib import urlretrieve
+from urllib import urlretrieve, URLopener
+import ssl
 from subprocess import call
 import os
 import zipfile
 
+# Unfortunately NESP SSL certificate doesn't play nice with Python 2.7 on Windows. Tried hard to fix this the proper way but gave up.
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+opener = URLopener(context = ctx)
+
 print("Downloading sample data, please wait")
 filename = 'sample.zip'
-urlretrieve('https://nesp-dev1.coesra.org.au/sample.zip', filename)
+opener.retrieve('https://nesp-dev1.coesra.org.au/sample.zip', filename)
 
 print("Extracting")
 zip_ref = zipfile.ZipFile(filename, 'r')
