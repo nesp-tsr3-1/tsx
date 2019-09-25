e0f3c5c8f13ddd7c7ab04522a0f22b8f1767e14a
James Watmuff <james@planticle.com.au>
2019-09-25 21:23:10 +1000

    Update data source import based on new MASTER LIST file

--- tsx/import_data_source.py
+++ tsx/import_data_source.py
@@ -11,7 +11,7 @@ def main():
 
 	parser = argparse.ArgumentParser(description='Import data source information')
 	parser.add_argument('--relax', action='store_true', dest='relax', help="Ignore invalid source ids")
-	parser.add_argument('filename', type=str, help='Processing method file (CSV)')
+	parser.add_argument('filename', type=str, help='Data source file  (aka master list) (CSV)')
 	args = parser.parse_args()
 
 	session = get_session()
@@ -24,18 +24,22 @@ def main():
 			data = {
 				'source_id': row['SourceID'],
 				'taxon_id': row['TaxonID'],
-				'data_agreement_id': row['DataAgreement'] or None,
-				'objective_of_monitoring_id': row['ObjectiveOfMonitoring'] or None,
-				'absences_recorded': row['AbsencesRecorded'] or 1,
-				'standardisation_of_method_effort_id': row['StandardisationOfMethodEffort'] or None,
-				'consistency_of_monitoring_id': row['ConsistencyOfMonitoring'] or None,
+				'data_agreement_id': row['AgreementSigned'] or None,
+				'objective_of_monitoring_id': lookup(row, 'ObjectiveOfMonitoring'),
+				'absences_recorded': get_bool(row, 'AbsencesRecorded', True, unknown_value_default=True),
+				'standardisation_of_method_effort_id': lookup(row, 'StandardisationOfMethodEffort'),
+				'consistency_of_monitoring_id': lookup(row, 'ConsistencyOfMonitoring'),
 				'start_year': row['StartYear'] or None,
 				'end_year': row['EndYear'] or None,
-				'exclude_from_analysis': row['Exclude'] or False,
-				'suppress_aggregated_data': row['SuppressAggregatedData'] or False,
+				'exclude_from_analysis': get_bool(row, 'NotInIndex', False, unknown_value_default=True),
+				'suppress_aggregated_data': get_bool(row, 'SuppressAggregatedDataUntil', False, unknown_value_default=True),
 				'authors': row['Authors'],
-				'provider': row['Provider']
+				'provider': row['SourceProvider']
 			}
+
+			# In relaxed mode, silently skip rows without SourceID value
+			if args.relax and row['SourceID'].strip() in ('', 'NULL'):
+				continue
 
 			r = session.execute("SELECT 1 FROM source WHERE id = :id", { 'id': data['source_id'] }).fetchall()
 			if len(r) == 0:
@@ -79,6 +83,58 @@ def main():
 
 
 	session.commit()
+
+
+LOOKUPS = {
+	'ObjectiveOfMonitoring': {
+		'Monitoring for targeted conservation management': 4,
+		'Monitoring for general conservation management – ‘surveillance’ monitoring.': 3,
+		'Baseline monitoring': 2,
+		'Monitoring for community engagement': 1
+	},
+	'ConsistencyOfMonitoring': {
+		'Balanced; all (or virtually all) sites surveyed in each year sampled (no, or virtually no, site turnover)': 4,
+		'Imbalanced (low turnover); sites surveyed consistently through time as established, but new sites are added to program with time': 3,
+		'Imbalanced (high turnover); new sites are surveyed with time, but monitoring of older sites is often not maintained': 2,
+		'Highly Imbalanced (very high turnover); different sites surveyed in different sampling periods. Sites are generally not surveyed consistently through time (highly biased)': 1
+	},
+	'StandardisationOfMethodEffort': {
+		'Pre-defined sites/plots surveyed repeatedly through time using a single standardised method and effort across the whole monitoring program': 6,
+		'Pre-defined sites/plots surveyed repeatedly through time with methods and effort standardised within site units, but not across program - i.e. different sites surveyed have different survey effort/methods': 5,
+		'Pre-defined sites/plots surveyed repeatedly through time with varying methods and effort': 4,
+		'Data collection using standardised methods and effort but surveys not site-based (i.e. surveys spatially ad-hoc). Post-hoc site grouping possible - e.g. a lot of fixed area/time searches conducted within a region but not at pre-defined sites': 3,
+		'Data collection using standardised methods and effort but surveys not site-based (i.e. surveys spatially ad-hoc). Post-hoc site grouping not possible': 2,
+		'Unstandardised methods/effort, surveys not site-based': 1
+	}
+}
+
+def get_bool(row, column, default=None, unknown_value_default=None):
+	raw_value = row[column].strip()
+	value = raw_value.lower()
+
+	if value in ('1', 'yes', 'true', 'y', 't'):
+		return True
+	if value in ('0', 'no', 'false', 'n', 'f'):
+		return False
+	if value in ('', 'na', 'null'):
+		return default
+	else:
+		log.warn("Unknown value for %s: '%s', defaulting to %s" % (column, raw_value, unknown_value_default))
+		return unknown_value_default
+
+def lookup(row, column):
+	value = row[column].strip()
+	lookup = LOOKUPS[column]
+
+	if value in ('', 'NA', '0'):
+		return None
+	elif value.isdigit() and int(value) in lookup.values():
+		return value
+	elif value in lookup:
+		return lookup[value]
+	else:
+		log.warn("Unknown value for %s: '%s', defaulting to None" % (column, value))
+		return None
 
 if __name__ == '__main__':
 	main()
