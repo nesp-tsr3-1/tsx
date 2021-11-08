3ca7aac9b21918232df1455ba8c9667ed2e15b59
James Watmuff <james@Jamess-MBP.home.gateway>
2021-11-08 20:57:56 +1100

    Update packer script

--- setup/ubuntu-setup-tsx-deps.sh
+++ setup/ubuntu-setup-tsx-deps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e # Abort on error
+set -x # Echo each command before running
+
 # Allow tsx to sudo without password
 echo "tsx ALL=NOPASSWD: ALL" > /etc/sudoers.d/tsx
 
@@ -16,6 +19,7 @@ EOF
 hostnamectl set-hostname tsx
 
 # Setup R APT repository - https://www.digitalocean.com/community/tutorials/how-to-install-r-on-ubuntu-18-04
+apt-get update
 apt-get install -y software-properties-common gnupg
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
@@ -23,7 +27,6 @@ add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran
 apt-get update
 
 apt-get install -y mysql-server python python-pip virtualenv r-base git samba libxslt1-dev
-
 
 # Set MySQL default character encoding to utf8
 sudo tee -a /etc/mysql/mysql.conf.d/encoding-utf8.cnf > /dev/null <<EOF
@@ -38,7 +41,7 @@ sudo service mysql restart
 # For some reason I seem to have to install these one by one
 apt-get install -y libssl-dev
 apt-get install -y libgit2-dev
-apt-get install -y libcurl4-openssl-dev
+#apt-get install -y libcurl4-openssl-dev # Note: This uninstall libgit2-dev because libgit2-dev is not compatible with it... it wants gnu_tls intead
 
 # Install R packages
 R --no-save <<EOF
@@ -46,5 +49,7 @@ install.packages("devtools")
 library(devtools)
 install.packages("ggplot2")
 install_github("nesp-tsr3-1/rlpi", dependencies=TRUE)
-install.packages('IRkernel')
+install.packages("IRkernel")
+install.packages("tidyverse")
+install.packages("data.table")
 EOF
