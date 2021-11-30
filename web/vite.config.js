9fc221be448bc666b6eab7207a9fb343212c9a53
James Watmuff <james@Jamess-MBP.home.gateway>
2021-11-30 11:58:19 +1100

    Vite config: build source maps

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/tsx/'
+  base: '/tsx/',
+  build: {
+   sourcemap: true,
+  },
 })
