eba4600ced8f99fa235c3b334535a0b3bf752456
James Watmuff <james@Jamess-MacBook-Pro.local>
2022-07-05 16:43:29 +1000

    Mail STARTTLS config - prevent use of DH ciphers

--- tsx/api/mail.py
+++ tsx/api/mail.py
@@ -2,6 +2,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from tsx.config import config
+import ssl
 
 smtp_host = config.get('smtp', 'host')
 smtp_port = config.getint('smtp', 'port')
@@ -13,7 +14,9 @@ smtp_sender = config.get('smtp', 'default_sender')
 def send_email(email_address, subject, message):
 	s = smtplib.SMTP(smtp_host, port=smtp_port)
 	if smtp_use_starttls:
-		s.starttls()
+		ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+		ssl_context.set_ciphers('DEFAULT:!DH')
+		s.starttls(context=ssl_context)
 	s.login(smtp_username, smtp_password)
 
 	msg = MIMEMultipart()
