4f5a95fb754958e911df63cd0c665d25ae5bc95e
James Watmuff <james@planticle.com.au>
2024-05-21 13:03:17 +1000

    Add sample data to repository

---
+++ sample-data/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO `source` (id, source_type_id, provider, description, notes) VALUES
+	(1,1,NULL,'Houtman Abrolhos Lesser Noddy',NULL),
+	(2,2,NULL,'Southern Rufous Scrub-bird (New England)',NULL),
+	(3,2,NULL,'Mainland Ground Parrot (NSW)',NULL),
+	(4,NULL,'BirdLife Australia','Birdata',NULL);
