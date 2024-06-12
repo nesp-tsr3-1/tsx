c5bbaaea49ca5eb9cdc7ea9b9593b707cd62739c
James Watmuff <james@planticle.com.au>
2024-06-12 16:48:12 +1000

    Fix dataset details page breaking if Data Details field is null

--- web/src/util.js
+++ web/src/util.js
@@ -308,9 +308,13 @@ export function deepEquals(a, b) {
 
 export function generateCitation(authors, details, provider) {
   let year = new Date().getFullYear();
-  authors = authors.trim().replace(/\.$/, '') || "<Author(s)>";
-  details = details.trim().replace(/\.$/, '') || "<Data Details>"
-  provider = provider.trim().replace(/\.$/, '') || "<Data Provider>"
+  function fix(str) {
+    return (str || "").trim().replace(/\.$/, '')
+  }
+
+  authors = fix(authors) || "<Author(s)>";
+  details = fix(details) || "<Data Details>"
+  provider = fix(provider) || "<Data Provider>"
 
   return `${authors} (${year}). ${details}. ${provider}. Aggregated for the Australian Threatened Species Index, an output of the NESP Threatened Species Recovery Hub and operated by the Terrestrial Ecosystem Research Network, The University of Queensland.`
 }
