d7ea81309601dae80fe0a1dec02bbb4437f84ab4
James Watmuff <james@planticle.com.au>
2026-04-14 16:11:28 +1000

    Update workflow test script to include permutations, remove old type 2 steps, and add a description for each step.

--- setup/test-workflow.sh
+++ setup/test-workflow.sh
@@ -2,25 +2,52 @@
 
 set -e
 
+echo IMPORT TAXON LIST
 python -m tsx.import_taxa sample-data/TaxonList.xlsx
-python -m tsx.importer --type 1 -c sample-data/type_1_sample.csv
-python -m tsx.importer --type 2 -c sample-data/type_2_sample.csv
-python -m tsx.import_region sample-data/spatial/Regions.shp
 
+echo IMPORT TYPE 1 DATA
+python -m tsx.importer --type 1 -c sample-data/type_1_sample.csv
+
+echo IMPORT TYPE 2 DATA
+python -m tsx.importer --type 2 -c sample-data/type_2_sample.csv
+
+echo IMPORT REGIONS
+python -m tsx.import_region sample-data/spatial/Regions-simplified.shp
+
+echo IMPORT PROCESSING METHODS
 python -m tsx.import_processing_method sample-data/processing_method.csv
+
+echo TYPE 1 DATA AGGREGATION
 python -m tsx.process -c t1_aggregation
+
+echo IMPORT INCIDENTAL SIGHTINGS
 python -m tsx.import_incidental sample-data/incidental_sightings.csv
+
+echo IMPORT SPECIES RANGES
 python -m tsx.import_range sample-data/spatial/species-range
+
+echo GENERATE ALPHA HULLS
 python -m tsx.process -c alpha_hull
-python -m tsx.process -c range_ultrataxon
-python -m tsx.import_t2_site sample-data/spatial/t2_site.shp
-python -m tsx.import_grid sample-data/spatial/10min_mainland.shp
-python -m tsx.process -c pseudo_absence
-python -m tsx.process -c response_variable
+
+echo TYPE 2 DATA AGGREGATION
+python -m tsx.process -c t2_aggregation
+
+echo CALCULATE SPATIAL REPRESENTATIVENESS
 python -m tsx.process -c spatial_rep
+
+echo IMPORT DATA SOURCE METADATA
 python -m tsx.import_data_source sample-data/data_source.csv
+
+echo FILTER TIME SERIES
 python -m tsx.process -c filter_time_series
+
+echo EXPORT TIME SERIES
 python -m tsx.process export_lpi
-python -m tsx.process export_lpi --filter
+
+echo GENERATE PERMUTATIONS
+python -m tsx.run_permutations data/export/lpi.csv data/export/results-test.db 2015
+
+echo EXPORT TIME SERIES AND TAXA FOR VISUALISER
+python -m tsx.export_results data/export/lpi.csv data/export/results-test.db
 
 echo "WORKFLOW TEST SUCCESSFUL"
